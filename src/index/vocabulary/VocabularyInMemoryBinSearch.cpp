b19736a7481079880b90190e80e7ae5c4b0e377b
marvin <marvin.stoetzel@mailbox.org>
2026-08-22 22:16:57 +0200

    refactor(vocabulary): encapsulate multi-source batch assembly into MultiSourceVocabBatchAssembler

--- src/index/vocabulary/VocabularyInMemoryBinSearch.cpp
+++ src/index/vocabulary/VocabularyInMemoryBinSearch.cpp
@@ -1,12 +1,6 @@
-// Copyright 2024 - 2026, The QLever Authors, in particular:
-//
-// 2024 - 2026 Johannes Kalmbach <johannes.kalmbach@gmail.com>, UFR
-// 2026        Marvin Stoetzel <stoetzem@email.uni-freiburg.de>, UFR
-//
-// UFR = University of Freiburg, Chair of Algorithms and Data Structures
-//
-// You may not use this file except in compliance with the Apache 2.0 License,
-// which can be found in the `LICENSE` file at the root of the QLever project.
+// Copyright 2024, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Johannes Kalmbach<joka921> (johannes.kalmbach@gmail.com)
 
 #include "index/vocabulary/VocabularyInMemoryBinSearch.h"
 
