1349574284e4374d31f9794a8559ee63b9001fb3
marvin <marvin.stoetzel@mailbox.org>
2026-08-22 20:39:49 +0200

    fix(index): add section separators to new DecoderMultiplexer methods

--- src/index/vocabulary/CompressionWrappers.h
+++ src/index/vocabulary/CompressionWrappers.h
@@ -108,6 +108,7 @@ struct DecoderMultiplexer {
     ENABLE_CLANG_WARNINGS
   }
 
+  // ______________________________________________________________
   // Return the maximum number of output bytes needed to decompress
   // `compressed` with `decoderIndex`.
   [[nodiscard]] size_t maxDecompressedSize(std::string_view compressed,
