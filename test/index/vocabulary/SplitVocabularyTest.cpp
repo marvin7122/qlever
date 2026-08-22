339a18176c697a1f6271b3f72f33feb835f185da
marvin <marvin.stoetzel@mailbox.org>
2026-08-22 20:50:00 +0200

    fix: repair vocabulary batch CI failures

--- test/index/vocabulary/SplitVocabularyTest.cpp
+++ test/index/vocabulary/SplitVocabularyTest.cpp
@@ -532,6 +532,8 @@ TEST(Vocabulary, SplitVocabularyLookupBatchMatchesItemAt) {
   vocabulary_test::assertLookupResultMatchesVocabularyAtIndices(
       sv, sv.lookupBatch(oneMarker), oneMarker);
   sv.close();
+  ad_utility::deleteFile(filename);
+  ad_utility::deleteFile(absl::StrCat(filename, ".a"), false);
 }
 
 // _____________________________________________________________________________
