8af05ca970e23cbcfb1a922ba0f7f44150237dd2
marvin <marvin.stoetzel@mailbox.org>
2026-08-23 18:39:47 +0200

    feat(backports): add src/backports/string.h header

---
+++ src/backports/string.h
@@ -0,0 +1,41 @@
+// Copyright 2026 The QLever Authors, in particular:
+//
+// 2026 Marvin Stoetzel <stoetzem@email.uni-freiburg.de>, UFR
+//
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
+//
+// You may not use this file except in compliance with the Apache 2.0 License,
+// which can be found in the `LICENSE` file at the root of the QLever project.
+
+#ifndef QLEVER_SRC_BACKPORTS_STRING_H
+#define QLEVER_SRC_BACKPORTS_STRING_H
+
+#include <string>
+#include <utility>
+
+#include "backports/concepts.h"
+#include "util/Exception.h"
+
+namespace ql {
+
+// Backport of C++23's `std::basic_string::resize_and_overwrite` as a free
+// function that takes the string as the first parameter.
+CPP_template(typename CharT, typename Traits, typename Allocator,
+             typename Operation)(
+    requires ql::concepts::invocable<Operation, CharT*, size_t>) void
+resize_and_overwrite(std::basic_string<CharT, Traits, Allocator>& str,
+                     size_t count, Operation&& op) {
+#if defined(__cpp_lib_string_resize_and_overwrite) && \
+    __cpp_lib_string_resize_and_overwrite >= 202110L
+  str.resize_and_overwrite(count, std::forward<Operation>(op));
+#else
+  str.resize(count);
+  const size_t newSize = op(str.data(), count);
+  AD_CONTRACT_CHECK(newSize <= count);
+  str.erase(newSize);
+#endif
+}
+
+}  // namespace ql
+
+#endif  // QLEVER_SRC_BACKPORTS_STRING_H
