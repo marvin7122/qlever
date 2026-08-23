8e3e038914d0f12a66875ecaf33b6f172c8215fc
Marvin Stoetzel <marvin.stoetzel@mailbox.org>
2026-08-23 22:44:43 +0200

    test(vocabulary): remove duplicate includes in SplitVocabularyTest

--- test/index/vocabulary/SplitVocabularyTest.cpp
+++ test/index/vocabulary/SplitVocabularyTest.cpp
@@ -14,12 +14,10 @@
 #include <utility>
 #include <variant>
 
-#include "../../util/GTestHelpers.h"
-#include "./VocabularyTestHelpers.h"
+#include "VocabularyTestHelpers.h"
 #include "backports/StartsWithAndEndsWith.h"
 #include "index/vocabulary/SplitVocabularyImpl.h"
 #include "index/vocabulary/Vocabulary.h"
-#include "index/vocabulary/VocabularyTestHelpers.h"
 #include "index/vocabulary/VocabularyType.h"
 #include "util/GTestHelpers.h"
 
