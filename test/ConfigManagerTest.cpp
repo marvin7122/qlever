32a8aab559f933e248bf3f4d1b74fc0fcfc27b4c
Marvin Stoetzel <marvin.stoetzel@mailbox.org>
2026-08-24 02:33:11 +0200

    style: format ConfigManager test assertion

--- test/ConfigManagerTest.cpp
+++ test/ConfigManagerTest.cpp
@@ -940,10 +940,9 @@ TEST(ConfigManagerTest, ParseShortHandTest) {
   checkOption(noChange, noChangeInt, true, 10);
 
   // Multiple key value pairs with the same key are not allowed.
-  AD_EXPECT_THROW_WITH_MESSAGE(
-      ad_utility::ConfigManager::parseShortHand(
-          R"(complicatedKey:42, complicatedKey:43)"),
-      ::testing::ContainsRegex("'complicatedKey'"));
+  AD_EXPECT_THROW_WITH_MESSAGE(ad_utility::ConfigManager::parseShortHand(
+                                   R"(complicatedKey:42, complicatedKey:43)"),
+                               ::testing::ContainsRegex("'complicatedKey'"));
 
   // Final test: Is there an exception, if we try to parse the wrong syntax?
   ASSERT_ANY_THROW(ad_utility::ConfigManager::parseShortHand(
