260caa062ea095c2a80f251a8d2b45eef31c190a
marvin <marvin.stoetzel@mailbox.org>
2026-08-25 22:38:38 +0000

    fix(review): remediate rule:correctness

--- src/util/FsstCompressor.h
+++ src/util/FsstCompressor.h
@@ -134,7 +134,6 @@ class FsstDecoder {
     return result;
   }
 
-  // Duplicate decompress(std::string_view) definition removed.
 
   // ___________________________________________________________________________
   // Allow this type to be trivially serializable,
