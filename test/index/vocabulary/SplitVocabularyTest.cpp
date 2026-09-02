f338817bc7cb9129ba1058285d3036bb28148bf9
Marvin Stoetzel <marvin.stoetzel@mailbox.org>
2026-09-02 17:16:59 +0200

    fix(test): restore valid comment syntax in SplitGeoVocab test

--- test/index/vocabulary/SplitVocabularyTest.cpp
+++ test/index/vocabulary/SplitVocabularyTest.cpp
@@ -74,11 +74,11 @@ const VocabularyType geoSplitVocabType{
 
 // _____________________________________________________________________________
 TEST(Vocabulary, SplitGeoVocab) {
-  partitioning and result merging directly.check : Is a geo literal
-      ? ASSERT_EQ(SGV::getMarkerForWord(
-                      "\"POLYGON((1 2, 3 4))\""
-                      "^^<http://www.opengis.net/ont/geosparql#wktLiteral>"),
-                  1);
+  // Check: Is a geo literal?
+  ASSERT_EQ(SGV::getMarkerForWord(
+                "\"POLYGON((1 2, 3 4))\""
+                "^^<http://www.opengis.net/ont/geosparql#wktLiteral>"),
+            1);
   ASSERT_EQ(SGV::getMarkerForWord(
                 "\"LINESTRING(1 2, 3 4)\""
                 "^^<http://www.opengis.net/ont/geosparql#wktLiteral>"),
