34206e85a77e6c9fa3205606ac27855a6dd2444b
marvin <marvin.stoetzel@mailbox.org>
2026-09-03 21:17:43 +0000

    fix(review): remediate rule:accidental-formatting

--- src/index/vocabulary/VocabularyTypes.h
+++ src/index/vocabulary/VocabularyTypes.h
@@ -831,8 +831,7 @@ class WordAndIndex {
   std::optional<uint64_t> previousIndex_ = std::nullopt;
 
  public:
-  // ___________________________________________________________________________
-  // Query for the special `end` semantics.
+    // Query for the special `end` semantics.
   bool isEnd() const { return !wordAndIndex_.has_value(); }
 
   // ___________________________________________________________________________
