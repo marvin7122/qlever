701c4ea973f57085ece991d84890bfa4ac571c49
marvin <marvin.stoetzel@mailbox.org>
2026-09-03 20:06:07 +0000

    fix(review): remediate rule:comment-format

--- src/engine/MonomorphicSerializers.h
+++ src/engine/MonomorphicSerializers.h
@@ -573,7 +573,7 @@ decltype(auto) dispatch2Col(ColumnType c0, ColumnType c1, Visitor&& visitor,
   }
 }
 
-// Fast-path dispatch for 3-column schemas (standard RDF triples and graph tables)
+// Dispatch three-column schemas through the fast path.
 template <typename Visitor, typename... Args>
 decltype(auto) dispatch3Col(ColumnType c0, ColumnType c1, ColumnType c2,
                             Visitor&& visitor, Args&&... args) {
