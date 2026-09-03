bb2daf1781239df49e81818db0193f0bb59320e4
marvin <marvin.stoetzel@mailbox.org>
2026-09-03 20:06:00 +0000

    fix(review): remediate rule:redundant-comment

--- benchmark/SimdEscapeBenchmark.cpp
+++ benchmark/SimdEscapeBenchmark.cpp
@@ -78,8 +78,7 @@ LiteralDataset generateRealisticLiteralDataset(size_t targetBytes = 100 * 1024 *
       }
     }
 
-    // Literal closing quote
-    literal.push_back('"');
+        literal.push_back('"');
 
     // 25% chance of language tag
     if (escapeProbDist(rng) < 25) {
