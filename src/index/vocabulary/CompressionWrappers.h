9a9c2f882f04e25ac24c97ce1b7edc6d998a62f7
marvin <marvin.stoetzel@mailbox.org>
2026-09-03 08:29:20 +0000

    fix(review): remediate rule:accidental-formatting

--- src/index/vocabulary/CompressionWrappers.h
+++ src/index/vocabulary/CompressionWrappers.h
@@ -1,11 +1,6 @@
-// Copyright 2024 - 2026, The QLever Authors, in particular:
-//
-// 2024 - 2026 Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>, UFR
-// 2026        Marvin Stoetzel <stoetzem@email.uni-freiburg.de>, UFR
-//
-// UFR = University of Freiburg, Chair of Algorithms and Data Structures
-//
-// You may not use this file except in compliance with the Apache 2.0 License,
+//  Copyright 2024, University of Freiburg,
+//  Chair of Algorithms and Data Structures.
+//  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
 // which can be found in the `LICENSE` file at the root of the QLever project.
 
 #ifndef QLEVER_SRC_INDEX_VOCABULARY_COMPRESSIONWRAPPERS_H
