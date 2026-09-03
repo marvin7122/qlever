0d6e4ecaa85b0d4cfe0ec81bac50965671d782e1
Marvin Stoetzel <marvin.stoetzel@mailbox.org>
2026-09-03 09:10:30 +0200

    feat(export): 16-entry branchless LUT type dispatcher (Optimization 15)

---
+++ test/engine/BranchlessTypeDispatcherTest.cpp
@@ -0,0 +1,117 @@
+// Copyright 2026, The QLever Authors, in particular:
+// 2026 Marvin Stoetzel <stoetzem@email.uni-freiburg.de>, UFR
+//
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
+//
+// You may not use this file except in compliance with the Apache 2.0 License,
+// which can be found in the `LICENSE` file at the root of the QLever project.
+
+#include <gtest/gtest.h>
+
+#include <array>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "engine/BranchlessTypeDispatcher.h"
+#include "global/Id.h"
+#include "global/ValueId.h"
+
+using namespace ql::engine;
+
+TEST(BranchlessTypeDispatcherTest, FormatIri) {
+  std::array<char, 256> buffer{};
+  auto id = ValueId::makeFromVocabIndex(VocabIndex::make(42));
+  std::string_view rawTerm = "http://example.org/resource";
+
+  char* end = BranchlessTypeDispatcher::dispatchTermFormat(
+      id, rawTerm, buffer.data(), BranchlessTypeDispatcher::defaultLut());
+  std::string_view result(buffer.data(), end - buffer.data());
+  EXPECT_EQ(result, "<http://example.org/resource>");
+}
+
+TEST(BranchlessTypeDispatcherTest, FormatLiteral) {
+  std::array<char, 256> buffer{};
+  auto id = ValueId::makeFromTextRecordIndex(TextRecordIndex::make(10));
+  std::string_view rawTerm = "Hello World";
+
+  char* end = BranchlessTypeDispatcher::dispatchTermFormat(
+      id, rawTerm, buffer.data(), BranchlessTypeDispatcher::defaultLut());
+  std::string_view result(buffer.data(), end - buffer.data());
+  EXPECT_EQ(result, "\"Hello World\"");
+}
+
+TEST(BranchlessTypeDispatcherTest, FormatInteger) {
+  std::array<char, 256> buffer{};
+  auto id = ValueId::makeFromInt(123456);
+
+  char* end = BranchlessTypeDispatcher::dispatchTermFormat(
+      id, "", buffer.data(), BranchlessTypeDispatcher::defaultLut());
+  std::string_view result(buffer.data(), end - buffer.data());
+  EXPECT_EQ(result, "\"123456\"^^<http://www.w3.org/2001/XMLSchema#integer>");
+}
+
+TEST(BranchlessTypeDispatcherTest, FormatDouble) {
+  std::array<char, 256> buffer{};
+  auto id = ValueId::makeFromDouble(3.14159);
+
+  char* end = BranchlessTypeDispatcher::dispatchTermFormat(
+      id, "", buffer.data(), BranchlessTypeDispatcher::defaultLut());
+  std::string_view result(buffer.data(), end - buffer.data());
+  EXPECT_TRUE(result.starts_with("\"3.14159"));
+  EXPECT_TRUE(
+      result.ends_with("\"^^<http://www.w3.org/2001/XMLSchema#double>"));
+}
+
+TEST(BranchlessTypeDispatcherTest, FormatBoolean) {
+  std::array<char, 256> buffer{};
+  auto idTrue = ValueId::makeFromBool(true);
+  auto idFalse = ValueId::makeFromBool(false);
+
+  char* end1 = BranchlessTypeDispatcher::dispatchTermFormat(
+      idTrue, "", buffer.data(), BranchlessTypeDispatcher::defaultLut());
+  EXPECT_EQ(std::string_view(buffer.data(), end1 - buffer.data()),
+            "\"true\"^^<http://www.w3.org/2001/XMLSchema#boolean>");
+
+  char* end2 = BranchlessTypeDispatcher::dispatchTermFormat(
+      idFalse, "", buffer.data(), BranchlessTypeDispatcher::defaultLut());
+  EXPECT_EQ(std::string_view(buffer.data(), end2 - buffer.data()),
+            "\"false\"^^<http://www.w3.org/2001/XMLSchema#boolean>");
+}
+
+TEST(BranchlessTypeDispatcherTest, FormatBlankNode) {
+  std::array<char, 256> buffer{};
+  auto id = ValueId::makeFromBlankNodeIndex(BlankNodeIndex::make(9876));
+
+  char* end = BranchlessTypeDispatcher::dispatchTermFormat(
+      id, "", buffer.data(), BranchlessTypeDispatcher::defaultLut());
+  std::string_view result(buffer.data(), end - buffer.data());
+  EXPECT_EQ(result, "_:bn9876");
+}
+
+TEST(BranchlessTypeDispatcherTest, FormatTurtleCompact) {
+  std::array<char, 256> buffer{};
+  auto idInt = ValueId::makeFromInt(42);
+
+  char* end = BranchlessTypeDispatcher::dispatchTermFormat(
+      idInt, "", buffer.data(), BranchlessTypeDispatcher::turtleLut());
+  std::string_view result(buffer.data(), end - buffer.data());
+  EXPECT_EQ(result, "42");
+}
+
+TEST(BranchlessTypeDispatcherTest, BatchFormatting) {
+  std::vector<ValueId> ids = {
+      ValueId::makeFromVocabIndex(VocabIndex::make(1)),
+      ValueId::makeFromInt(100),
+      ValueId::makeFromBlankNodeIndex(BlankNodeIndex::make(5)),
+      ValueId::makeFromBool(true)};
+  std::vector<std::string_view> rawTerms = {"http://example.org/pred", "", "",
+                                            ""};
+  std::array<char, 1024> buffer{};
+
+  size_t bytesWritten = BranchlessTypeDispatcher::dispatchBatchTermFormat(
+      ids, rawTerms, buffer.data(), BranchlessTypeDispatcher::turtleLut());
+
+  std::string_view output(buffer.data(), bytesWritten);
+  EXPECT_EQ(output, "<http://example.org/pred>100_:bn5true");
+}
