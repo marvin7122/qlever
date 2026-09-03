99125d3fb601d8d662d8cf4b0fccb7fd8190ec6c
Marvin Stoetzel <marvin.stoetzel@mailbox.org>
2026-09-03 19:09:05 +0200

    test(export-v2): use finalize on ScatterGatherChunkBuilder in ExportEngineV2 and tests

--- test/ExportEngineV2Test.cpp
+++ test/ExportEngineV2Test.cpp
@@ -3,7 +3,7 @@
 //
 // UFR = University of Freiburg, Chair of Algorithms and Data Structures
 //
-// You may not use this file except in compliance with the Apache 2.0 License,
+// You may not use this file except in compliance with the License,
 // which can be found in the `LICENSE` file at the root of this project.
 
 #include <gtest/gtest.h>
@@ -27,7 +27,10 @@ TEST(ExportEngineV2Test, SerializeTableChunkCsv) {
   ScatterGatherChunkBuilder builder;
 
   auto chunk = ExportEngineV2::serializeTableChunk(table, localVocab, RowFormat::Csv, builder);
-  EXPECT_EQ(chunk.view(), "42,100\n7,999\n");
+  std::string output;
+  output.resize(chunk.totalBytes());
+  chunk.copyTo(ql::span<char>(output.data(), output.size()));
+  EXPECT_EQ(output, "42,100\n7,999\n");
 }
 
 TEST(ExportEngineV2Test, SerializeTableChunkTsv) {
@@ -40,22 +43,37 @@ TEST(ExportEngineV2Test, SerializeTableChunkTsv) {
   ScatterGatherChunkBuilder builder;
 
   auto chunk = ExportEngineV2::serializeTableChunk(table, localVocab, RowFormat::Tsv, builder);
-  EXPECT_EQ(chunk.view(), "1\t2\n3\t4\n");
+  std::string output;
+  output.resize(chunk.totalBytes());
+  chunk.copyTo(ql::span<char>(output.data(), output.size()));
+  EXPECT_EQ(output, "1\t2\n3\t4\n");
 }
 
 TEST(ExportEngineV2Test, PipelineMorselIntegration) {
-  AsyncChunkPipeline pipeline{8};
+  AsyncChunkPipeline<std::string> pipeline{
+      AsyncChunkPipelineConfig{.capacity_ = 8, .runtimeEnabled_ = true}};
 
   ScatterGatherChunkBuilder builder1;
   builder1.appendCopy("chunk_1\n");
-  auto chunk1 = builder1.build();
-  pipeline.push(0, std::string(chunk1.view()));
+  auto chunk1 = std::move(builder1).finalize();
+  std::string s1;
+  s1.resize(chunk1.totalBytes());
+  chunk1.copyTo(ql::span<char>(s1.data(), s1.size()));
+  EXPECT_EQ(pipeline.push(std::move(s1)), AsyncChunkPipeline<std::string>::PushResult::Ok);
 
   ScatterGatherChunkBuilder builder2;
   builder2.appendCopy("chunk_2\n");
-  auto chunk2 = builder2.build();
-  pipeline.push(1, std::string(chunk2.view()));
+  auto chunk2 = std::move(builder2).finalize();
+  std::string s2;
+  s2.resize(chunk2.totalBytes());
+  chunk2.copyTo(ql::span<char>(s2.data(), s2.size()));
+  EXPECT_EQ(pipeline.push(std::move(s2)), AsyncChunkPipeline<std::string>::PushResult::Ok);
 
-  EXPECT_EQ(pipeline.pop(), "chunk_1\n");
-  EXPECT_EQ(pipeline.pop(), "chunk_2\n");
+  auto res1 = pipeline.pop();
+  ASSERT_TRUE(res1.has_value());
+  EXPECT_EQ(*res1, "chunk_1\n");
+
+  auto res2 = pipeline.pop();
+  ASSERT_TRUE(res2.has_value());
+  EXPECT_EQ(*res2, "chunk_2\n");
 }
