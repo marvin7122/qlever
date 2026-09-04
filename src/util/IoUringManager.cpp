cc628e2df364b23b72f4e530d3bd36cc5be84adf
Marvin Stoetzel <marvin.stoetzel@mailbox.org>
2026-09-04 23:27:35 +0200

    fix(watchdog): restore lambda opener dropped by D2, re-apply cqes decl lost in rebase (seq 1875)

--- src/util/IoUringManager.cpp
+++ src/util/IoUringManager.cpp
@@ -175,7 +175,7 @@ void IoUringPolicy::addBatch(int fd,
   }
   numInFlightReadRequestsPerBatch_[handle] = numReadRequestsToPerform;
 
-  
+  auto prepareOne = [&](size_t i) {
     io_uring_sqe* sqe = io_uring_get_sqe(&ring_);
     AD_CORRECTNESS_CHECK(sqe != nullptr);
 
@@ -258,7 +258,8 @@ void IoUringPolicy::drainAllReadyCqes() {
   };
   std::vector<RawCqe> raw;
   raw.reserve(ringSize_);
-    while (true) {
+  std::array<io_uring_cqe*, 64> cqes{};
+  while (true) {
     const unsigned n =
         io_uring_peek_batch_cqe(&ring_, cqes.data(), cqes.size());
     if (n == 0) {
