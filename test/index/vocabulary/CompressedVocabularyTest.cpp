3b3bdd5fbdd2bc1f7b80a83fcdf488a079c38ced
marvin <marvin.stoetzel@mailbox.org>
2026-09-04 23:07:06 +0000

    fix(review): design remediation

--- test/index/vocabulary/CompressedVocabularyTest.cpp
+++ test/index/vocabulary/CompressedVocabularyTest.cpp
@@ -143,7 +143,7 @@ TYPED_TEST(CompressedVocabularyF, LookupBatchMatchesAccessOperator) {
   const std::array<size_t, 7> indices{4, 1, 0, 3, 1, 2, 4};
   auto result = vocab.lookupBatch(indices);
   assertLookupResultMatchesVocabularyAtIndices(vocab, result, indices);
-  EXPECT_ANY_THROW(vocab.lookupBatch(ql::span<const size_t>{}));
+  EXPECT_TRUE(vocab.lookupBatch(ql::span<const size_t>{}).empty());
 }
 
 // _______________________________________________________
