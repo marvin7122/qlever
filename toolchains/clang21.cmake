fda9bf6f060c2ee765ed9e13e670d6288d9c29ac
Marvin Stoetzel <marvin.stoetzel@mailbox.org>
2026-09-04 21:30:11 +0200

    fix(ci): Set macOS deployment target to 13.3 for std::to_chars support (PR #99)

--- toolchains/clang21.cmake
+++ toolchains/clang21.cmake
@@ -1,5 +1,6 @@
 # CMake toolchain file for using `clang++-21`
 set(CMAKE_C_COMPILER clang-21)
 set(CMAKE_CXX_COMPILER clang++-21)
+set(CMAKE_OSX_DEPLOYMENT_TARGET "13.3")
 # See https://github.com/llvm/llvm-project/issues/76515
 set(CMAKE_CXX_FLAGS "-Wno-deprecated-declarations")
