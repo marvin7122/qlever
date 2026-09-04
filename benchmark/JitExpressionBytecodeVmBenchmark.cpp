7c67d898a401ddfb37b4371d8626f66dc45528c4
Marvin Stoetzel <marvin.stoetzel@mailbox.org>
2026-09-04 14:58:30 +0200

    fix(jit): align AsmJit compiler method calls to v1.21 API

--- benchmark/JitExpressionBytecodeVmBenchmark.cpp
+++ benchmark/JitExpressionBytecodeVmBenchmark.cpp
@@ -126,32 +126,32 @@ int main() {
   asmjit::x86::Compiler cc(&code);
 
   // Define function signature
-  asmjit::FuncNode* func = cc.addFunc(asmjit::FuncSignature::build<size_t, const int64_t*, const int64_t*, size_t>());
+  asmjit::FuncNode* func = cc.add_func(asmjit::FuncSignature::build<size_t, const int64_t*, const int64_t*, size_t>());
 
-  asmjit::x86::Gp ptr0 = cc.newUIntPtr("ptr0");
-  asmjit::x86::Gp ptr1 = cc.newUIntPtr("ptr1");
-  asmjit::x86::Gp count = cc.newUIntPtr("count");
-  asmjit::x86::Gp matches = cc.newUInt64("matches");
-  asmjit::x86::Gp idx = cc.newUIntPtr("idx");
+  asmjit::x86::Gp ptr0 = cc.new_gp_ptr("ptr0");
+  asmjit::x86::Gp ptr1 = cc.new_gp_ptr("ptr1");
+  asmjit::x86::Gp count = cc.new_gp_ptr("count");
+  asmjit::x86::Gp matches = cc.new_gp64("matches");
+  asmjit::x86::Gp idx = cc.new_gp_ptr("idx");
 
-  func->setArg(0, ptr0);
-  func->setArg(1, ptr1);
-  func->setArg(2, count);
+  func->set_arg(0, ptr0);
+  func->set_arg(1, ptr1);
+  func->set_arg(2, count);
 
   cc.xor_(matches, matches);
   cc.xor_(idx, idx);
 
-  asmjit::Label loopStart = cc.newLabel();
-  asmjit::Label loopEnd = cc.newLabel();
+  asmjit::Label loopStart = cc.new_label();
+  asmjit::Label loopEnd = cc.new_label();
 
   cc.bind(loopStart);
   cc.cmp(idx, count);
   cc.jge(loopEnd);
 
   // Load col0[idx] and col1[idx]
-  asmjit::x86::Gp v0 = cc.newInt64("v0");
-  asmjit::x86::Gp v1 = cc.newInt64("v1");
-  asmjit::x86::Gp sum = cc.newInt64("sum");
+  asmjit::x86::Gp v0 = cc.new_gp64("v0");
+  asmjit::x86::Gp v1 = cc.new_gp64("v1");
+  asmjit::x86::Gp sum = cc.new_gp64("sum");
 
   cc.mov(v0, asmjit::x86::qword_ptr(ptr0, idx, 3)); // scale 8 = 1 << 3
   cc.mov(v1, asmjit::x86::qword_ptr(ptr1, idx, 3));
@@ -161,7 +161,7 @@ int main() {
 
   // Compare > 100
   cc.cmp(sum, 100);
-  asmjit::x86::Gp matchBit = cc.newUInt64("matchBit");
+  asmjit::x86::Gp matchBit = cc.new_gp64("matchBit");
   cc.setg(matchBit.r8());
   cc.movzx(matchBit, matchBit.r8());
   cc.add(matches, matchBit);
@@ -171,7 +171,7 @@ int main() {
 
   cc.bind(loopEnd);
   cc.ret(matches);
-  cc.endFunc();
+  cc.end_func();
 
   cc.finalize();
 
@@ -181,7 +181,7 @@ int main() {
   auto c1 = std::chrono::high_resolution_clock::now();
   double compileUs = std::chrono::duration<double, std::micro>(c1 - c0).count();
 
-  if (err || !nativeFn) {
+  if (err != asmjit::Error::kOk || !nativeFn) {
     std::cerr << "AsmJit compilation failed: code=" << static_cast<unsigned int>(err) << "\n";
     return 1;
   }
