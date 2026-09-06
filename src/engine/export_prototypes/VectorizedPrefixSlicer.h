3f8199f069fb93bafefd2d72ecc90796bc87081e
marvin <marvin.stoetzel@mailbox.org>
2026-09-06 19:11:12 +0000

    fix(review): remediate skill:parnas-modularity:PARNAS-REPRESENTATION

--- src/engine/export_prototypes/VectorizedPrefixSlicer.h
+++ src/engine/export_prototypes/VectorizedPrefixSlicer.h
@@ -40,7 +40,7 @@ constexpr std::string_view kXmlSchemaPrefix = "http://www.w3.org/2001/XMLSchema#
 class VectorizedPrefixTable {
  public:
     // Stores a prefix aligned to 16 bytes with its length.
-  struct PrefixEntry {
+  alignas(16) struct PrefixEntry {
     alignas(16) char data[48];
     size_t length;
   };
