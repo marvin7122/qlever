93059bdd7729c20b653f26b11575ff0f8753b713
marvin <marvin.stoetzel@mailbox.org>
2026-09-06 20:03:47 +0000

    fix(review): remediate rule:comment-format

--- src/engine/ScatterGatherArenaStreamer.h
+++ src/engine/ScatterGatherArenaStreamer.h
@@ -81,7 +81,7 @@ class ScatterGatherChunk : public ad_utility::WithInvariants<ScatterGatherChunk>
  public:
   ScatterGatherChunk() = default;
 
-  // Restricted constructor via Passkey from ScatterGatherChunkStreamer.
+    // Construct via Passkey from ScatterGatherChunkStreamer.
   ScatterGatherChunk(Passkey, std::vector<struct iovec> iovecs,
                      std::vector<char> headerBuffer, size_t totalBytes,
                      size_t numTriples, size_t zeroCopySpansCount,
