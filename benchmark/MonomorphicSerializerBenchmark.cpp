e9ab8ebd00fb58fee36b7fa4c15f9794121b603f
marvin <marvin.stoetzel@mailbox.org>
2026-09-06 17:20:15 +0000

    fix(review): remediate rule:comment-precision

--- benchmark/MonomorphicSerializerBenchmark.cpp
+++ benchmark/MonomorphicSerializerBenchmark.cpp
@@ -36,7 +36,7 @@
 #include "util/http/MediaTypes.h"
 
 // _____________________________________________________________________________
-// Memory allocation tracker for verifying zero-allocation hot loops.
+// Memory allocation tracker to verify that hot loops perform zero allocations.
 struct AllocationTracker {
   static inline std::atomic<bool> enabled_{false};
   static inline std::atomic<size_t> count_{0};
