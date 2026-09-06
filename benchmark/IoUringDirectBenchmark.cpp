f24f52aaf973381e3ce79718f22555e7aa00414f
marvin <marvin.stoetzel@mailbox.org>
2026-09-06 19:53:46 +0000

    fix(review): remediate rule:includes

--- benchmark/IoUringDirectBenchmark.cpp
+++ benchmark/IoUringDirectBenchmark.cpp
@@ -25,7 +25,7 @@
 
 // Optional inclusion of QLever benchmark infrastructure
 #if __has_include("../benchmark/infrastructure/Benchmark.h")
-#include "../benchmark/infrastructure/Benchmark.h"
+#include "benchmark/infrastructure/Benchmark.h"
 #define QLEVER_HAS_BENCHMARK_INFRASTRUCTURE 1
 #endif
 
