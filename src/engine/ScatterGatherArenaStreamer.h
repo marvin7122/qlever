fe75b0295e57b94c77cf5d6d4db4b7bd5fd0ea4a
marvin <marvin.stoetzel@mailbox.org>
2026-09-06 22:25:30 +0000

    fix(review): remediate rule:missing-doc

--- src/engine/ScatterGatherArenaStreamer.h
+++ src/engine/ScatterGatherArenaStreamer.h
@@ -70,7 +70,7 @@ class ScatterGatherChunk : public ad_utility::WithInvariants<ScatterGatherChunk>
  private:
     std::vector<struct iovec> iovecs_; // Descriptors for external arena memory pages
     std::vector<char> headerBuffer_; // Owned buffer for delimiters and short tokens
-  size_t totalBytes_ = 0;
+    size_t totalBytes_ = 0; // Total byte length of the assembled chunk
   size_t numTriples_ = 0;
   size_t zeroCopySpansCount_ = 0;
   size_t zeroCopyBytes_ = 0;
