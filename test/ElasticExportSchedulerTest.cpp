0dffd3d6dc0e2ea40159d3d6fcc5d6e14b0559d8
marvin <marvin.stoetzel@mailbox.org>
2026-09-12 17:30:33 +0200

    fix(ci): clang-format ElasticExportSchedulerTest.cpp

--- test/ElasticExportSchedulerTest.cpp
+++ test/ElasticExportSchedulerTest.cpp
@@ -11,13 +11,10 @@
 #include <stdexcept>
 #include <string>
 #include <thread>
-
-#include "engine/export_v2/ElasticExportScheduler.h"
-#include "engine/export_v2/ExportJobState.h"
-#include "util/http/websocket/QueryId.h"
 #include <vector>
 
 #include "engine/export_v2/ElasticExportScheduler.h"
+#include "engine/export_v2/ExportJobState.h"
 #include "util/GTestHelpers.h"
 #include "util/http/websocket/QueryId.h"
 
