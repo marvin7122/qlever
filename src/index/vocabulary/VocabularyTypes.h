174ececba4e60274c2235583aa7b6df898f9f26b
marvin <marvin.stoetzel@mailbox.org>
2026-09-17 23:37:08 +0200

    fix(vocab): remove stale InvariantStatefulClass static_asserts (PR 78)
    The Invariants framework was removed as non-MVP, but four static_asserts
    (plus one comment) referencing the deleted ad_utility::InvariantStatefulClass
    concept were left behind in VocabularyTypes.h and the split-vocabulary and
    vocabulary-types tests, breaking every TU that includes VocabularyTypes.h.

--- src/index/vocabulary/VocabularyTypes.h
+++ src/index/vocabulary/VocabularyTypes.h
@@ -577,9 +577,6 @@ class MultiSourceVocabBatchAssembler {
   }
 };
 
-static_assert(
-    ad_utility::InvariantStatefulClass<MultiSourceVocabBatchAssembler>);
-
 // _____________________________________________________________________________
 // Paired lookup data for one vocabulary marker: for each position `i` in the
 // arrays, `underlyingIndices[i]` is the index to look up, and
@@ -631,8 +628,6 @@ class MarkerIndicesAndPositions {
     return underlyingIndices_.size();
   }
 };
-
-static_assert(ad_utility::InvariantStatefulClass<MarkerIndicesAndPositions>);
 
 // _____________________________________________________________________________
 // Paired lookup data for each of the `NumVocabs` underlying vocabularies,
