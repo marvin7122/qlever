06a0f5a07e01a112d81be60a8ebb5c6a256d7173
marvin <marvin.stoetzel@mailbox.org>
2026-09-19 21:11:51 +0200

    Fix include path and wire test+benchmark into CMake
    engine/export_prototypes/FastExportStreamFormatter.h does not exist;
    the header lives at engine/FastExportStreamFormatter.h. Fix the path
    in the PR's three files and register ScatterGatherArenaStreamerTest
    and ScatterGatherBenchmark so CI actually builds them.

--- test/ScatterGatherArenaStreamerTest.cpp
+++ test/ScatterGatherArenaStreamerTest.cpp
@@ -16,7 +16,7 @@
 #include "backports/span.h"
 #include "engine/ConstructTypes.h"
 #include "engine/ScatterGatherArenaStreamer.h"
-#include "engine/export_prototypes/FastExportStreamFormatter.h"
+#include "engine/FastExportStreamFormatter.h"
 #include "global/Constants.h"
 
 using namespace ql::export_streaming;
