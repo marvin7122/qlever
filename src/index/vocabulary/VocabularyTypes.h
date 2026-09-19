7735dc80d015d117459f0c6261d9d73119d2e24d
marvin <marvin.stoetzel@mailbox.org>
2026-09-19 22:46:27 +0200

    Remove stale InvariantStatefulClass asserts (framework removed as non-MVP)

--- src/index/vocabulary/VocabularyTypes.h
+++ src/index/vocabulary/VocabularyTypes.h
@@ -523,9 +523,6 @@ class MultiSourceVocabBatchAssembler {
   }
 };
 
-static_assert(
-    ad_utility::InvariantStatefulClass<MultiSourceVocabBatchAssembler>);
-
 // _____________________________________________________________________________
 // Paired lookup data for one vocabulary marker: for each position `i` in the
 // arrays, `underlyingIndices[i]` is the index to look up, and
@@ -577,8 +574,6 @@ class MarkerIndicesAndPositions {
     return underlyingIndices_.size();
   }
 };
-
-static_assert(ad_utility::InvariantStatefulClass<MarkerIndicesAndPositions>);
 
 // _____________________________________________________________________________
 // Paired lookup data for each of the `NumVocabs` underlying vocabularies,
