ebabbb5079e4e507913774e0f31291eeebc527a1
marvin <marvin.stoetzel@mailbox.org>
2026-09-20 08:44:46 +0200

    fix(ci): clang-format-18 style fixes (PR 98)

--- src/engine/MonomorphicSerializers.h
+++ src/engine/MonomorphicSerializers.h
@@ -335,7 +335,6 @@ class DynamicRowSerializer {
     AD_CONTRACT_CHECK(!schema_.empty());
   }
 
-
   [[nodiscard]] const std::vector<ColumnType>& schema() const noexcept {
     return schema_;
   }
