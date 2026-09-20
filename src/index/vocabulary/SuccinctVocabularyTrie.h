0d091b6ac3bf5880a32d3e5582ddce68c85aea40
marvin <marvin.stoetzel@mailbox.org>
2026-09-20 07:43:18 +0200

    fix(review): drop unused backports/span.h include

--- src/index/vocabulary/SuccinctVocabularyTrie.h
+++ src/index/vocabulary/SuccinctVocabularyTrie.h
@@ -15,8 +15,6 @@
 #include <string_view>
 #include <vector>
 
-#include "backports/span.h"
-
 namespace ql::index::vocab {
 
 // _____________________________________________________________________________
