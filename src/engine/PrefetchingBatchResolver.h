f757d1790405c7f93c5e7b796d21d46ead7c5679
Muse Code <muse-code@local>
2026-09-22 21:18:58 +0200

    fix(engine): qualify LiteralOrIriView as ql::exportIds (PR 88)
    The unqualified name in ql::engine::prefetch no longer resolves: the view
    lives in ad_utility::triple_component and is aliased as
    ql::exportIds::LiteralOrIriView. Fixes the 'use of undeclared identifier'
    error in PrefetchingBatchResolverTest builds.

--- src/engine/PrefetchingBatchResolver.h
+++ src/engine/PrefetchingBatchResolver.h
@@ -159,7 +159,8 @@ class PrefetchingBatchResolver {
 
       results[pos] = ql::exportIds::literalOrIriToStringAndType<
           removeQuotesAndAngleBrackets, returnOnlyLiterals>(
-          LiteralOrIriView::fromStringRepresentation(word), escapeFunction);
+          ql::exportIds::LiteralOrIriView::fromStringRepresentation(word),
+          escapeFunction);
     }
   }
 
